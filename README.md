85e354bef8ef7cb25700a0d8823d5bb4c3f73ad8
Walt Beaman <walter.g.beaman@outlook.com>
2023-04-26 15:51:15 -0500

    Add app icon and rework UI/UX. (#5)
    * Initial code commit
    
    * Added screenshot.
    
    * Added line break.
    
    * Added line break.
    
    * Fixed pie chart legend text color.
    
    * Added TODOs for needed work, cleaned up files/filenames, and updated README.md.
    
    * Added TODOs and tweaked spacing for README.md.
    
    * Add external library links to README.md.
    
    * Adjusted wording in README.md
    
    * Adjusted image placement in README.md
    
    * Adjusted image placement in README.md
    
    * Tweak wording in README.md.
    
    * Move overall mood rating and increase font size.
    
    * Fix typo and add icon to README.md.
    
    * Adjust app icon position.

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
+<<<<<<< HEAD
+# <img src="icon.png?raw=true" width="50" height="50" style="position: relative;top: 20px;">Twitter Mood Tracker
+
+Find the current mood of recent tweets for a given topic on Twitter.
+<br /><hr />
+Required Libraries:
+
+<ul>
+    <li><a href="https://github.com/tweepy/tweepy">Tweepy</a></li>
+    <li><a href="https://github.com/sloria/TextBlob">TextBlob</a></li>
+    <li><a href="https://wiki.python.org/moin/PyQt">PyQt5</a></li>
+</ul>
+Use <code>pip install &lt;packagenamehere&gt;</code> to install required packages.<br />
+Tweepy is used to collect the tweets, TextBlob analyzes the sentiment (or "mood") of the tweets, and PyQt5 provides the UI elements.<br />
+<br /><hr />
+<img src="screenshot-01.png?raw=true">
+=======
 # Twitter Mood Tracker
 Find the current mood of a given topic on Twitter.
 <br />
 <br />
 ![Main Window](screenshot-01.png?raw=true "Main Window")
+>>>>>>> bc016585d58c2d50021f81f473d0c152c578374a
---
+++ darkmode.py
@@ -0,0 +1,53 @@
+DARK_STYLE = """
+    QWidget {
+        background-color: #2b2b2b;
+        color: #ffffff;
+        font-size: 12px;
+    }
+    QLineEdit {
+        background-color: #414141;
+        border: 1px solid #5a5a5a;
+        border-radius: 4px;
+        padding: 2px 4px;
+    }
+    QPushButton {
+        background-color: #3e3e3e;
+        border: 1px solid #5a5a5a;
+        border-radius: 4px;
+        padding: 4px;
+    }
+    QPushButton:hover {
+        background-color: #5a5a5a;
+    }
+    #overallMoodLabel {
+        font-size: 18px;
+        font-weight: bold;
+        color: white;
+    }
+    QTextEdit {
+        background-color: #414141;
+        border: 1px solid #5a5a5a;
+        border-radius: 4px;
+        padding: 4px;
+    }
+    QScrollBar:vertical {
+        background-color: #2b2b2b;
+        width: 14px;
+        margin: 15px 3px 15px 3px;
+    }
+    QScrollBar::handle:vertical {
+        background-color: #5a5a5a;
+        min-height: 30px;
+    }
+    QScrollBar::add-line:vertical {
+        border: none;
+        background: none;
+    }
+    QScrollBar::sub-line:vertical {
+        border: none;
+        background: none;
+    }
+    QChart {
+        background-color: #2b2b2b;
+    }
+"""
